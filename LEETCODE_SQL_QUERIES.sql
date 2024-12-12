ebbc411a2194362feb87282aae6e0b2be0f69247
omkar burute <buruteomkar90@gmail.com>
2024-12-12 13:01:00 +0530

    SOLVED SQL_LEETCODE Q_NO 1757

---
+++ LEETCODE_SQL_QUERIES.sql
@@ -0,0 +1,7 @@
+/*QUESTION LINK
+https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-plan-v2&envId=top-sql-50
+
+Question:Write a solution to find the ids of products that are both low fat and recyclable.Return the result table in any order.*/
+
+select product_id from Products where low_fats = "Y" and recyclable ="Y";
+
