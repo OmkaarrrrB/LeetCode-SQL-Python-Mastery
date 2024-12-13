93f06978e4e52f831938a4800ffd9e52799f1ec3
omkar burute <buruteomkar90@gmail.com>
2024-12-13 19:37:26 +0530

    Question No 1280 (also added some beautification to entire script)

--- LEETCODE_SQL_QUERIES.sql
+++ LEETCODE_SQL_QUERIES.sql
@@ -6,7 +6,12 @@ https://leetcode.com/problems/recyclable-and-low-fat-products/?envType=study-pla
 Question 1757
 Write a solution to find the ids of products that are both low fat and recyclable.Return the result table in any order
 */
-select product_id from Products where low_fats = "Y" and recyclable ="Y";
+SELECT 
+    product_id
+FROM
+    Products
+WHERE
+    low_fats = 'Y' AND recyclable = 'Y';
 
 /*
 QUESTION LINK
@@ -17,7 +22,17 @@ Find the names of the customer that are not referred by the customer with id = 2
 Return the result table in any order.
 */
 
-select name from Customer where id not in(select id from Customer where referee_id=2);
+SELECT 
+    name
+FROM
+    Customer
+WHERE
+    id NOT IN (SELECT 
+            id
+        FROM
+            Customer
+        WHERE
+            referee_id = 2);
 
 /*
 Question Link
@@ -29,7 +44,13 @@ it has an area of at least three million (i.e., 3000000 km2), or
 it has a population of at least twenty-five million (i.e., 25000000).
 Write a solution to find the name, population, and area of the big countries.
 */
-select name ,population, area from World where area >=3000000 or population >=25000000;
+SELECT 
+    name, population, area
+FROM
+    World
+WHERE
+    area >= 3000000
+        OR population >= 25000000;
 
 
 /*
@@ -52,7 +73,12 @@ Question No:1683
 Write a solution to find the IDs of the invalid tweets. The tweet is invalid if the number of characters used in the content of the tweet is strictly greater than 15.
 Return the result table in any order.
 */
-select tweet_id from Tweets where length(content)>15;
+SELECT 
+    tweet_id
+FROM
+    Tweets
+WHERE
+    LENGTH(content) > 15;
 
 
 -- JOINS --
@@ -65,7 +91,12 @@ Question No:1378
 Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
 Return the result table in any order.
 */
-select u.unique_id,e.name from EmployeeUNI u right join Employees e on u.id=e.id; 
+SELECT 
+    u.unique_id, e.name
+FROM
+    EmployeeUNI u
+        RIGHT JOIN
+    Employees e ON u.id = e.id; 
 
 
 /*
@@ -75,7 +106,12 @@ https://leetcode.com/problems/product-sales-analysis-i/description/?envType=stud
 Question No 1068
 Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
 */
-select p.product_name, s.year, s.price from Sales s join Product p on s.product_id=p.product_id;
+SELECT 
+    p.product_name, s.year, s.price
+FROM
+    Sales s
+        JOIN
+    Product p ON s.product_id = p.product_id;
 
 
 /*
@@ -153,3 +189,27 @@ Question no:577
 Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
 */
 select e.name,b.bonus from Employee e left join Bonus b on e.empId=b.empId where bonus<1000 or bonus is null;
+
+
+/*
+Question Link
+https://leetcode.com/problems/students-and-examinations/description/?envType=study-plan-v2&envId=top-sql-50
+
+Question No:1280
+Write a solution to find the number of times each student attended each exam.
+Return the result table ordered by student_id and subject_name.
+*/
+SELECT 
+    s.student_id,
+    s.student_name,
+    su.subject_name,
+    COUNT(e.subject_name) AS attended_exams
+FROM
+    Students s
+        CROSS JOIN
+    Subjects su
+        LEFT JOIN
+    Examinations e ON s.student_id = e.student_id
+        AND su.subject_name = e.subject_name
+GROUP BY s.student_id , su.subject_name
+ORDER BY s.student_id , su.subject_name;
