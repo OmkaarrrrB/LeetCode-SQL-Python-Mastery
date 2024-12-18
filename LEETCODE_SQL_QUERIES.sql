41df26d5f4f62c26f2fc5cd69b783b094818bffe
omkar burute <buruteomkar90@gmail.com>
2024-12-18 17:21:41 +0530

    Question No 1251

--- LEETCODE_SQL_QUERIES.sql
+++ LEETCODE_SQL_QUERIES.sql
@@ -269,4 +269,4 @@ FROM
         LEFT JOIN
     UnitsSold u ON u.product_id = p.product_id
         AND u.purchase_date BETWEEN p.start_date AND p.end_date
-GROUP BY p.product_id;
+GROUP BY p.product_id ;
