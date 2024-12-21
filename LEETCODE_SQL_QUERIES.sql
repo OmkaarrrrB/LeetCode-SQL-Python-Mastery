e24bff927867f83ec6105a6895576d637e0b664e
omkar burute <buruteomkar90@gmail.com>
2024-12-21 22:24:47 +0530

    Question no 1211

--- LEETCODE_SQL_QUERIES.sql
+++ LEETCODE_SQL_QUERIES.sql
@@ -310,3 +310,24 @@ FROM
     Register r ON u.user_id = r.user_id
 GROUP BY contest_id
 ORDER BY percentage DESC , contest_id ASC;
+
+/*
+Question Link:
+https://leetcode.com/problems/queries-quality-and-percentage/description/?envType=study-plan-v2&envId=top-sql-50
+
+Question No:1211
+We define query quality as:
+The average of the ratio between query rating and its position.
+We also define poor query percentage as:
+The percentage of all queries with rating less than 3.
+Write a solution to find each query_name, the quality and poor_query_percentage.
+Both quality and poor_query_percentage should be rounded to 2 decimal places.
+Return the result table in any order.
+*/
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position), 2) AS quality,
+    ROUND(SUM(rating < 3) / COUNT(rating) * 100, 2) AS poor_query_percentage
+FROM
+    Queries
+GROUP BY query_name;
