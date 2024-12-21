76975afb798d0e9a81295a03ca4742b930ffddc5
omkar burute <buruteomkar90@gmail.com>
2024-12-21 22:44:04 +0530

    Question no 1193

--- LEETCODE_SQL_QUERIES.sql
+++ LEETCODE_SQL_QUERIES.sql
@@ -331,3 +331,23 @@ SELECT
 FROM
     Queries
 GROUP BY query_name;
+
+
+/*
+Question Link:
+https://leetcode.com/problems/monthly-transactions-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+Question No:1193
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+Return the result table in any order.
+*/
+SELECT 
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(id) AS trans_count,
+    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM
+    Transactions
+GROUP BY month , country;
