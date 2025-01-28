5057844a5d85ca9cf51d6b829a1d1965922abe33
omkar burute <buruteomkar90@gmail.com>
2025-01-28 12:57:02 +0530

    Question No 1141

--- LEETCODE_SQL_QUERIES.sql
+++ LEETCODE_SQL_QUERIES.sql
@@ -458,3 +458,20 @@ HAVING COUNT(DISTINCT product_key) = (SELECT
     FROM
         Product);
 
+
+/*
+Question Link:
+https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/?envType=study-plan-v2&envId=top-sql-50
+
+Question No:1141
+Write a solution to find the daily active user count for a period of 30 days ending 2019-07-27 inclusively. A user was active on someday if they made at least one activity on that day.
+Return the result table in any order.
+*/
+SELECT 
+    activity_date AS day,
+    COUNT(DISTINCT (user_id)) AS active_users
+FROM
+    Activity
+GROUP BY activity_date
+HAVING activity_date > '2019-06-27'
+    AND activity_date < '2019-07-31';
